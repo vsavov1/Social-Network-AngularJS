7d151417a51f0e2e1be2f9305d0ac642b29d424a
Veselin Savov <veselin.savov1@gmail.com>
2015-05-21 20:35:27 +0300

    Delete HomeController.js and replaced with MainController.js

--- app/js/controllers/HomeController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.controller('HomeController',
-    function ($scope, authService) {
-        $scope.isLoggedIn = authService.isLoggedIn();
-        $scope.isAnonymous = authService.isAnonymous();
-    }
-);
